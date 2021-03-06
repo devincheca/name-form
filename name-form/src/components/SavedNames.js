6a2773e2698931c1dd72b537f884c85fbc2d683e
Devin Checa <devincheca@gmail.com>
2021-03-06 13:54:03 -0800

    more pieces are in place, next is the form then hapi

--- name-form/src/components/SavedNames.js
+++ name-form/src/components/SavedNames.js
@@ -1,11 +1,39 @@
-import './SavedNames.css';
+import { useState } from 'react';
+import './SavedNames.scoped.css';
 
 function SavedNames() {
+
+  const [names, setNames] = useState([])
+  const [isLoading, setLoading] = useState(true)
+
+  function getNames() {
+    if (isLoading) {
+      setTimeout(() => setLoading(false), 2000);
+      return 'Loading...';
+    }
+    if (names.length === 0) {
+      return 'No names saved';
+    }
+    return names.map(name => {
+      return (
+        <div className="row">
+          <div className="col s6">
+            {name.firstName}
+          </div>
+          <div className="col s6">
+            {name.lastName}
+          </div>
+        </div>
+      )
+    })
+  }
+
   return (
     <div className="App">
-      test saved names
+      { getNames() }
     </div>
   );
+
 }
 
 export default SavedNames;
