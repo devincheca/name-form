c13a7fcc94292b531e6848d8c1bd0bec3faeb204
Devin Checa <devincheca@gmail.com>
2021-03-06 20:02:43 -0800

    starting to refine the UI while I'm connecting things

--- name-form/src/components/SavedNames/SavedNames.js
+++ name-form/src/components/SavedNames/SavedNames.js
@@ -8,13 +8,22 @@ function SavedNames() {
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
+    if (isLoading) {
+      try { fetchNames() } catch(error) {
+        console.trace(error)
+        setTimeout(fetchNames, 5000)
+      }
+    }
+  })
+
+  function fetchNames() {
     const fetcher = new NameFetcher()
     fetcher.callback = (res) => {
       setLoading(false)
       setNames(res.names)
     }
     fetcher.getNames()
-  })
+  }
 
   function getNames() {
     if (isLoading) {
@@ -35,10 +44,10 @@ function SavedNames() {
       return (
         <div key={i} className="row">
           <div className="col s6">
-            {name.firstName}
+            First Name: {name.firstName}
           </div>
           <div className="col s6">
-            {name.lastName}
+            Last Name: {name.lastName}
           </div>
         </div>
       )
--- name-form/src/components/components.js
+++ name-form/src/components/components.js
@@ -1,6 +1,8 @@
 import FormInput from './FormInput/FormInput'
+import Header from './Header/Header'
 import SavedNames from './SavedNames/SavedNames'
 export {
   FormInput,
+  Header,
   SavedNames
 }
